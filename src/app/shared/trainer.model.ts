49b3ffd006b81ae82094c7c74c8a69db377c2c71
tayyabsyed <tayyab.syedd@gmail.com>
2018-12-21 15:31:38 +0500

    before changes

---
+++ src/app/shared/trainer.model.ts
@@ -0,0 +1,12 @@
+export class Trainer {
+    name: string;
+    experience: string;
+    offers: string;
+    description: string;
+    rate: string;
+    time: string;
+    focus: string;
+
+    
+
+}
