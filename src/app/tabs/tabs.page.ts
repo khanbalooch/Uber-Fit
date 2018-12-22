dce8a9bc7b8c617fc1218d88236f09ec6574bc29
Ibrahim Khan <ibrahimkhan1122@gmail.com>
2018-12-22 17:55:34 +0500

    tabs styled

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -3,6 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss','tabs.page.css']
+  styleUrls: ['tabs.page.scss', 'tabs.page.css']
 })
-export class TabsPage {}
+export class TabsPage {
+
+  isClicked(tab){
+    //return true;
+  }
+
+  // tabSwitched(tab) {
+  //   console.log(tab);
+  // }
+
+
+}
