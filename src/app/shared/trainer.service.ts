e66b7337bb2b524444a8888dfc60f6560098dd73
Ibrahim Khan <ibrahimkhan1122@gmail.com>
2018-12-22 18:46:25 +0500

    discription less

--- src/app/shared/trainer.service.ts
+++ src/app/shared/trainer.service.ts
@@ -20,7 +20,7 @@ export class trainerService {
             '../../assets/images/cardio.jpg',
             '5 Years',
             'Chrismis Offer',
-            'Apart from being a certified Personal trainer through the American Council on Exercise.Erwin is also a fully qualified physiotharapist and a master Rehab traine. With his background.cription',
+            'Apart from being a certified Personal trainer through the American Council on Exercise.Erwin is also a fully qualified physiotharapist.',
             '$40',
             '1 hr',
             'Yoga'
@@ -32,7 +32,7 @@ export class trainerService {
             '../../assets/images/cf.jpg',
             '3 Years',
             'Exclusive Offer',
-            'Apart from being a certified Personal trainer through the American Council on Exercise.Erwin is also a fully qualified physiotharapist and a master Rehab traine. With his background.',
+            'Apart from being a certified Personal trainer through the American Council on Exercise.Erwin is also a fully qualified physiotharapist.',
             '$50',
             '1.5 hr',
             'Cross Fit'
@@ -44,7 +44,7 @@ export class trainerService {
             '../../assets/images/swt.png',
             '1 Years',
             'Limited Time Offer',
-            'Apart from being a certified Personal trainer through the American Council on Exercise.Erwin is also a fully qualified physiotharapist and a master Rehab traine. With his background.',
+            'Apart from being a certified Personal trainer through the American Council on Exercise.Erwin is also a fully qualified physiotharapist.',
             '$100',
             '0.5 hr',
             'Weight Loosing'
@@ -52,11 +52,11 @@ export class trainerService {
         ),
         new Trainer(
             '4',
-            'name 4',
+            'John Wick',
             '../../assets/images/wt.jpg',
             '2 Years',
             'Monthly Offer',
-            'Apart from being a certified Personal trainer through the American Council on Exercise.Erwin is also a fully qualified physiotharapist and a master Rehab traine. With his background.',
+            'Apart from being a certified Personal trainer through the American Council on Exercise.Erwin is also a fully qualified physiotharapist.',
             '$150',
             '1 hr',
             'Walking and Running'
