f6236cccebfbe891e362bb335c9b2a0b20f94a2b
Ibrahim Khan <ibrahimkhan1122@gmail.com>
2018-12-24 19:10:30 +0500

    backButton tbc

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Trainer } from '../shared/trainer.model';
+import { trainerService } from '../shared/trainer.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss','tab2.page.css']
 })
-export class Tab2Page {}
+export class Tab2Page implements OnInit {
+  trainers: Trainer[];
+  constructor(private trainerService: trainerService, private router: Router) { }
+  
+
+  ngOnInit() {
+    this.trainers = this.trainerService.getAllTrainers();
+  }
+
+  loadTrainer(trainer: any) {
+    this.trainerService.setTrainerID(trainer.id);
+    this.router.navigateByUrl('/loader');
+  }
+
+
+}
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Trainer } from '../shared/trainer.model';
+import { trainerService } from '../shared/trainer.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss', 'tab3.page.css']
 })
-export class Tab3Page {}
+export class Tab3Page implements OnInit {
+  trainers: Trainer[];
+  constructor(private trainerService: trainerService, private router: Router) { }
+
+
+  ngOnInit() {
+    this.trainers = this.trainerService.getAllTrainers();
+  }
+
+  loadTrainer(trainer: any) {
+    this.trainerService.setTrainerID(trainer.id);
+    this.router.navigateByUrl('/loader');
+  }
+
+
+}
