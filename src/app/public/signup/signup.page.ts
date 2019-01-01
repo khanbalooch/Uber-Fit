b220907e711b7a23d3b6afa884b5889bb2de5655
qsemantics <muh_ibrahim@outlook.com>
2019-01-01 21:10:43 +0500

    sign up implemented

--- src/app/public/signup/signup.page.ts
+++ src/app/public/signup/signup.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
@@ -8,78 +9,36 @@ import { Router } from '@angular/router';
 })
 export class SignupPage implements OnInit {// implements OnInit {
 
-  triedToSubmit = false;
-  errorMessages = {
-    username: {
-        required: 'Name required',
-        minlength: 'Please enter at least 4 characters.'
-    },
-    email: {
-        required: 'Email required',
-        email: 'Invalid email address'
-    },
-    password: {
-        required: 'Password required',
-        pattern: 'Invalid, please see rules'
-    }
-};
+
   signupDetails = { name: '', email: '', password: '' };
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
   ngOnInit() {}
+
+  signUp() {
+      console.log(this.signupDetails);
+    const httpHeaders = new HttpHeaders({
+        'Content-Type' : 'application/json',
+        'Cache-Control': 'no-cache'
+         });
+         let options = {
+        headers: httpHeaders
+         };
+      let signUpURL = 'http://apps.capbpm.com/UF/api/v1/person';
+        this.http.post(signUpURL, {
+            'firstName': this.signupDetails.name,
+            'email': this.signupDetails.email,
+            'password': this.signupDetails.password
+        }, options).subscribe(
+            data => {console.log(data);
+                this.router.navigate(['/login']);
+             },
+            error => {console.log(error); }
+        );
+  }
 
   gotoLogin() {
     console.log('going to login page');
     this.router.navigateByUrl('/login');
   }
-  /* constructor(private formBuilder: FormBuilder) {
-         this.form = formBuilder.group({
-             username: ['', [Validators.required, Validators.minLength(4)]],
-             email: ['', [Validators.required, Validators.email]],
-             password: ['', [Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z]).{6,}')]]
-         });
-     }
-
-     form: FormGroup;
-     formSettings = {
-         theme: 'ios',
-     };
-
-     triedToSubmit: boolean = false;
-
-     submit() {
-         this.triedToSubmit = true;
-         if (this.form.invalid) {
-             for (var control in this.form.controls) {
-                 this.form.controls[control].markAsDirty();
-                 this.form.controls[control].markAsTouched();
-             }
-         }
-     }
-
-     errorMessages = {
-         username: {
-             required: 'Name required',
-             minlength: 'Please enter at least 4 characters.'
-         },
-         email: {
-             required: 'Email required',
-             email: 'Invalid email address'
-         },
-         password: {
-             required: 'Password required',
-             pattern: 'Invalid, please see rules'
-         }
-     }
-
-     errorFor(field) {
-         var control = this.form.controls[field];
-         for (var validator in control.errors) {
-             if (control.errors[validator]) {
-                 return this.errorMessages[field][validator];
-             }
-         }
-         return null;
-     }*/
-
 }
