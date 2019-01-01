31c155e05a40c8426465627b19e4ce42540014fe
khanbalooch <ibrahimkhan1122@gmail.com>
2019-01-01 15:38:05 +0500

    current location client

--- src/app/pages/global/global.page.ts
+++ src/app/pages/global/global.page.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { GlobalService } from '../../shared/services/global.service';
+//import { GlobalService } from '../../shared/services/global.service';
+import { MapService } from '../../shared/services/map.service';
 
 @Component({
   selector: 'app-global',
   templateUrl: './global.page.html',
-  styleUrls: ['./global.page.scss','./global.page.css'],
+  styleUrls: ['./global.page.scss', './global.page.css'],
 })
 export class GlobalPage implements OnInit {
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+  lat: any;
+  lng: any;
 
-  constructor(private globalService: GlobalService) { }
-
+  constructor(private mapS: MapService) { }
   ngOnInit() {
+    this.mapS.getLocation().subscribe(res => {
+       this.lat = res.latitude;
+       this.lng = res.longitude;
+      console.log(res);
+    });
   }
 
 }
