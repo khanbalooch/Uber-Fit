0a09f4d63e75f133c875e473613b7c03c7ad5bcd
khanbalooch <muh_ibrahim@outlook.com>
2019-01-05 03:44:57 +0500

    homepage

--- src/app/public/signup/signup.page.ts
+++ src/app/public/signup/signup.page.ts
@@ -5,36 +5,38 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
-  styleUrls: ['./signup.page.scss','./signup.page.css'],
+  styleUrls: ['./signup.page.scss', './signup.page.css'],
 })
 export class SignupPage implements OnInit {// implements OnInit {
   //test
+  isTrainer: boolean;
 
-  signupDetails = { name: '', email: '', password: '',Cpassword: '' };
-  constructor(private router: Router, private http: HttpClient) {}
+  signupDetails = { name: '', email: '', password: '', Cpassword: '' };
+  constructor(private router: Router, private http: HttpClient) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
   signUp() {
-      console.log(this.signupDetails);
+    console.log(this.signupDetails);
     const httpHeaders = new HttpHeaders({
-        'Content-Type' : 'application/json',
-        'Cache-Control': 'no-cache'
-         });
-         let options = {
-        headers: httpHeaders
-         };
-      let signUpURL = 'http://apps.capbpm.com/UF/api/v1/person';
-        this.http.post(signUpURL, {
-            'firstName': this.signupDetails.name,
-            'email': this.signupDetails.email,
-            'password': this.signupDetails.password,
-            
-        }, options).subscribe(
-            data => {console.log(data);
-                this.router.navigate(['/login']);
-             },
-            error => {console.log(error); }
-        );
+      'Content-Type': 'application/json',
+      'Cache-Control': 'no-cache'
+    });
+    let options = {
+      headers: httpHeaders
+    };
+    let signUpURL = 'http://apps.capbpm.com/UF/api/v1/person';
+    this.http.post(signUpURL, {
+      'firstName': this.signupDetails.name,
+      'email': this.signupDetails.email,
+      'password': this.signupDetails.password,
+
+    }, options).subscribe(
+      data => {
+        console.log(data);
+        this.router.navigate(['/login']);
+      },
+      error => { console.log(error); }
+    );
   }
 
   gotoLogin() {
