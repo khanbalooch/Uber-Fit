05854daa307507451c8de57d139c0066ac733edf
qsemantics <muh_ibrahim@outlook.com>
2019-01-05 03:42:58 +0500

    signup validation almost complete

--- src/app/public/signup/signup.page.ts
+++ src/app/public/signup/signup.page.ts
@@ -1,16 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormArray, FormGroup, FormControl, Validators } from '@angular/forms';
+
+export function MustMatch(controlName: string, matchingControlName: string) {
+  return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+          // return if another validator has already found an error on the matchingControl
+          return;
+      }
+
+      // set error on matchingControl if validation fails
+      if (control.value !== matchingControl.value) {
+          matchingControl.setErrors({ mustMatch: true });
+      } else {
+          matchingControl.setErrors(null);
+      }
+  };
+}
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss','./signup.page.css'],
 })
-export class SignupPage implements OnInit {// implements OnInit {
-  //test
 
-  signupDetails = { name: '', email: '', password: '',Cpassword: '' };
+export class SignupPage implements OnInit {// implements OnInit {
+
+  signupDetails = { name: '', email: '', password: '', Cpassword: '' };
+  signupForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required ]),
+    cPassword: new FormControl('', [Validators.required]),
+    type: new FormControl('', [Validators.required])
+  });
+
   constructor(private router: Router, private http: HttpClient) {}
 
   ngOnInit() {}
@@ -41,4 +68,17 @@ export class SignupPage implements OnInit {// implements OnInit {
     console.log('going to login page');
     this.router.navigateByUrl('/login');
   }
+  get email() {
+    return this.signupForm.get('email');
+  }
+  get password() {
+    return this.signupForm.get('password');
+  }
+  get cPassword() {
+    return this.signupForm.get('cPassword');
+  }
+  get type() {
+    return this.signupForm.get('type');
+  }
+
 }
