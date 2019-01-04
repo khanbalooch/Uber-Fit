9386eb41e0a3915fe80b1fb17ba6605a38cbde98
khanbalooch <muh_ibrahim@outlook.com>
2019-01-05 04:39:37 +0500

    filter-component added

--- src/app/pages/filter/filter.page.ts
+++ src/app/pages/filter/filter.page.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.page.html',
-  styleUrls: ['./filter.page.scss'],
+  styleUrls: ['./filter.page.scss', './filter.page.css'],
 })
 export class FilterPage implements OnInit {
 
-  constructor() { }
+  constructor(public modalController: ModalController) { }
 
   ngOnInit() {
+  }
+  onFilterBtnClick() {
+    this.modalController.dismiss();
   }
 
 }
