991dc404c16cf371276befc03a349e65baa8e9f4
khanbalooch <muh_ibrahim@outlook.com>
2019-01-05 22:18:50 +0500

    filter page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -25,6 +25,7 @@ export class HomePage implements OnInit {
 
   ngOnInit() {
     this.getAllTrainers();
+
   }
   selectedTrainer(trainer) {
     this.trainerS.setSelectedTrainer(trainer);
@@ -47,7 +48,6 @@ export class HomePage implements OnInit {
       }, err => {
         this.error = err;
         console.log(err);
-
         loading.dismiss();
       });
   }
