07ca3a1c42fe07db6a265a4a885ceb125c3c8d0d
khanbalooch <muh_ibrahim@outlook.com>
2019-01-05 16:40:00 +0500

    trainers api added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,8 @@ import { ModalController } from '@ionic/angular';
 import { FilterPage } from '../pages/filter/filter.page';
 import { Router } from '@angular/router';
 
+import { LoadingController } from '@ionic/angular';
+import { trainerService } from '../shared/trainer.service';
 
 @Component({
   selector: 'app-home',
@@ -11,15 +13,49 @@ import { Router } from '@angular/router';
 })
 
 export class HomePage implements OnInit {
+  trainers: any;
+  error: any;
+  show: boolean = false;
   isShowSearchBar: boolean = false;
   isShowFilter: boolean = true;
 
-  constructor(private router: Router, public modalController: ModalController) {
-      console.log('In Home Page');
+  constructor(private router: Router, public modalController: ModalController, private trainerS: trainerService, private loadingController: LoadingController) {
+    //console.log('In Home Page');
   }
 
   ngOnInit() {
+    this.getAllTrainers();
   }
+  selectedTrainer(trainer) {
+    this.trainerS.setSelectedTrainer(trainer);
+    this.router.navigateByUrl('/trainer');
+  }
+
+  //getAllTrainers
+  async getAllTrainers() {
+    const loading = await this.loadingController.create({
+      message: 'Finding Trainers in your Area',
+      spinner: 'bubbles',
+      cssClass: 'tLoader'
+    });
+    await loading.present();
+    await this.trainerS.getAllTrainers()
+      .subscribe(res => {
+        console.log(res);
+        this.trainers = res;
+        loading.dismiss();
+      }, err => {
+        this.error = err;
+        console.log(err);
+
+        loading.dismiss();
+      });
+  }
+
+  isValidPic(url: any) {
+    return (/\.(gif|jpg|jpeg|tiff|png)$/i).test(url);
+  }
+
   onShowSearchbar() {
     this.isShowSearchBar = true;
   }
