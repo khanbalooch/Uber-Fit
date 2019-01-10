79ed1ab05e5ae0b7cf05ec51e17f970880fd2e59
khanbalooch <muh_ibrahim@outlook.com>
2019-01-10 18:12:19 +0500

    trainer page

--- src/app/trainer/trainer.page.ts
+++ src/app/trainer/trainer.page.ts
@@ -6,18 +6,37 @@ import { trainerService } from '../shared/trainer.service';
 @Component({
   selector: 'app-trainer',
   templateUrl: './trainer.page.html',
-  styleUrls: ['./trainer.page.scss','./trainer.page.css'],
+  styleUrls: ['./trainer.page.scss', './trainer.page.css'],
 })
 export class TrainerPage implements OnInit {
+  
   selectedTrainer: any;
-  constructor(private trainerService: trainerService, private router: Router) { }
+  isFav: boolean = false;
+  constructor(private trainerService: trainerService, private router: Router) {
+
+  }
+  galleryOpts = {
+    effect: 'flip',
+    slidesPerView: 'auto',
+    spaceBetween: 10
+  };
+
+  feedbackOpts = {
+    effect: 'flip',
+    slidesPerView: 'auto',
+    spaceBetween: 10
+  };
+  
   ngOnInit() {
+
     this.selectedTrainer = this.trainerService.getSelectedTrainer();
   }
 
   onBookClient() {
     this.router.navigateByUrl('book-client');
-  }
+  }  
+
+ 
 
 
 
